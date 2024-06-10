a568a5f648ecd8dbf5c022a1fe84b3d14ba44e3c
Ahmedfull02 <adnaneahmed2@gmail.com>
2024-06-10 17:28:51 +0100

    add saving and loading from a file

--- Students.py
+++ Students.py
@@ -1,9 +1,30 @@
+import json
+from os import write
 import tkinter
-from tkinter import END, ttk, messagebox
+from tkinter import END, Frame, ttk, messagebox
+from turtle import bgcolor
 
 # helps to show results as table
-from tabulate import tabulate
+from tabulate import tabulate # type: ignore
 
+
+# read list of studentss from the file
+def read_students():
+    # Open the file in read mode
+    with open('students.txt', 'r') as file:
+        s = json.load(file)
+    print("Dictionary is read from file succefully")
+    return s
+
+# Dictionary to store the data
+Students = read_students()
+
+# function to save the list to the file
+
+def save_students():
+    with open('students.txt','w') as file:
+        json.dump(Students,file)
+    messagebox.showinfo("Save","List saves succefully")
 
 # This Function removes the entries after an action
 def empty_entries():
@@ -11,52 +32,52 @@ def empty_entries():
     name_entry.delete(0,END)
 
 
-# This Function shows the list of students (Dictionnary)
+# This Function shows the list of studentss (Dictionnary)
 def show_students(): 
     result.delete(1.0,END)
     headers = ["ID", "Name"]
-    table = [[key, value['Name']] for key, value in Student.items()]
+    table = [[key, value['Name']] for key, value in Students.items()]
     result_text = tabulate(table, headers, tablefmt="plain")
     result.insert(END, result_text)
     
 
-# This Function adds students to the students list (Dictionnary)
-def add_student():
+# This Function adds studentss to the studentss list (Dictionnary)
+def add_students():
     id = ID_entry.get()
     Name = name_entry.get()
     id = id.upper()
     if Name == '':                              # verify entries if it is empty ,if so it shows an error message
         messagebox.showerror(title="Error", message="Please insert all entries .")
 
-    elif id in Student:
-        Student [id] = {'Name':Name}            # Updating an existing student
-        messagebox.showinfo(title="Information", message="Student is updated.")
+    elif id in Students:
+        Students [id] = {'Name':Name}            # Updating an existing students
+        messagebox.showinfo(title="Information", message="Students is updated.")
     else:
-        Student [id] = {'Name':Name}            # Adding a new student
-        messagebox.showinfo(title="Information", message="Student is added succesfuly.")
+        Students [id] = {'Name':Name}            # Adding a new students
+        messagebox.showinfo(title="Information", message="Students is added succesfuly.")
     empty_entries()
 
 
     
-def search_student():
+def search_students():
     id = ID_entry.get()
     id = id.upper()
     result.delete(1.0,END)
     if id == '':                                 # verify ID entry if it is empty ,if so it shows an error message  (Name is not necessary) 
         messagebox.showerror(title="Error", message="Please insert the ID .")
 
-    elif id in Student:
-        headers = ["ID", "Name"]                 # showing a message to user of students existing in the list
-        messagebox.showinfo(title="Information", message="Student is found.")
+    elif id in Students:
+        headers = ["ID", "Name"]                 # showing a message to user of studentss existing in the list
+        messagebox.showinfo(title="Information", message="Students is found.")
         table = []
-        for key , value in Student.items(): # Showing the student in text area
+        for key , value in Students.items(): # Showing the students in text area
             if key == id:
                 table.append([key, value['Name']])
         result_text = tabulate(table, headers, tablefmt="plain")
         result.insert(END, result_text)
         
     else:
-        messagebox.showinfo(title="Information", message="Student not found.")
+        messagebox.showinfo(title="Information", message="Students not found.")
     
     empty_entries()
     
@@ -65,41 +86,31 @@ def search_student():
 def delete_student():
     id = ID_entry.get()
     # confirmation message to alert user  
-    answer = messagebox.askquestion(title="Confirmation", message="Are you sure you want to delete the student?")
+    answer = messagebox.askquestion(title="Confirmation", message="Are you sure you want to delete the students?")
     if answer == "yes":
         if id == '':                    # verify ID entry if it is empty ,if so it shows an error message  (Name is not necessary)
-            messagebox.showerror(title="Error", message="Please insert ID of student.")
-        elif id not in Student:
-            messagebox.showerror(title="Error", message="Student not found")
+            messagebox.showerror(title="Error", message="Please insert ID of students.")
+        elif id not in Students:
+            messagebox.showerror(title="Error", message="Students not found")
         else :
-            for key in Student:
+            for key in Students:
                 if key == id:
-                    del Student[id] 
-                    messagebox.showinfo(title="Information", message="Student is deleted succesfuly.")
+                    del Students[id] 
+                    messagebox.showinfo(title="Information", message="Students is deleted succesfuly.")
                     show_students()
     else:
         messagebox.showinfo(title="Information", message="The action cancelled.")
     empty_entries()
         
 
-# the list of students
-Student = {
-    'E1' : {'Name':'John Johnson'},
-    'E2' : {'Name':'David Garcia'},
-    'E3' : {'Name':'Michael Miller'},
-    'E4' : {'Name':'James Davis'},
-    'E5' : {'Name':'Robert Brown'}
-}
-
 # Creating the window and title of window 
+
 
 root = tkinter.Tk()
 root.title('Students MANAGER')
 
-# text area that showes list
-
 result = tkinter.Text(root,height=10)
-result.grid(row=5,column=0,columnspan=2,padx=5,pady=5)
+result.grid(row=6,column=0,columnspan=2,padx=5,pady=5)
 
 # all labels 
 ID_label        = ttk.Label(root,text='ID : ')  
@@ -116,16 +127,23 @@ ID_entry.       grid(row=0,column=1,padx=40,pady=20)
 name_entry.     grid(row=1,column=1,padx=40,pady=20)
 
 # all buttons
-add_button      = ttk.Button(root,text='Add / modify a student'   ,command=add_student)   
-search_button   = ttk.Button(root,text='Search a student',command=search_student)
-delete_button   = ttk.Button(root,text='delete this student',command=delete_student)
+add_button      = ttk.Button(root,text='Add / modify a students'   ,command=add_students)   
+search_button   = ttk.Button(root,text='Search student',command=search_students)
+delete_button   = ttk.Button(root,text='delete student',command=delete_student)
 show_all_button = ttk.Button(root,text='Show list of students',command= lambda : show_students())
+save_button     = ttk.Button(root,text='Save list of students',command= save_students)
 
 add_button      .grid(row=3,column=0,padx=40,pady=20)
 search_button   .grid(row=3,column=1,padx=40,pady=20)
 delete_button   .grid(row=4,column=0,padx=40,pady=20)
 show_all_button .grid(row=4,column=1,padx=40,pady=20)
+save_button     .grid(row=5,columnspan=2,padx=40,pady=20)
 
+add_button      .configure()
+search_button   .configure()
+delete_button   .configure()
+show_all_button .configure()
+save_button     .configure()
 
 # function to show the window
 root.mainloop()
